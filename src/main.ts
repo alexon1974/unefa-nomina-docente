a6931d193e6ecb0818233243f9918568862880e1
Alex Suarez <alexon1974@gmail.com>
2026-08-01 23:06:51 -0400

    Install zone.js and update to modern application builder

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .then(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch((err) => {
-        console.error('Service Worker registration failed:', err);
-      });
-    }
-  })
   .catch((err) => console.error(err));
